d6610bd2151d88eb1bb5424e77a4ce7b143283bd
danielconde <danielconde9@gmail.com>
2021-02-18 22:14:05 +0000

    feat: add support to cli to provide a commit type

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -4,6 +4,9 @@ use nvim_conventional_commits::ConventionalCommitsHint;
 
 fn main() {
     let cwd = env::current_dir().unwrap();
-    let conventional_commits = ConventionalCommitsHint::new(&cwd, None);
+    let args: Vec<String> = env::args().collect();
+    let commit_type = args.get(1).map(|ct| ct.as_str());
+    let conventional_commits = ConventionalCommitsHint::new(&cwd, commit_type);
+
     println!("{}", conventional_commits.get_suggested_commit());
 }
