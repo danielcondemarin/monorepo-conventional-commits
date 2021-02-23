f2dcfe574b74e7bc88ea1ac6c90c9f10848b32d4
danielconde <danielconde9@gmail.com>
2021-02-23 21:20:13 +0000

    feat: add logging back in

--- src/bin/prepare-commit-msg.rs
+++ src/bin/prepare-commit-msg.rs
@@ -1,10 +1,14 @@
-use nvim_conventional_commits::{prepare_commit_msg::PrepareCommitMessage, DefaultScopeFinder};
+use nvim_conventional_commits::{
+    logger::Logger, prepare_commit_msg::PrepareCommitMessage, DefaultScopeFinder,
+};
 use std::{env, path::Path};
 
 fn main() {
     let cwd = env::current_dir().unwrap();
     let args: Vec<String> = env::args().collect();
     let commit_msg_file = args.get(1).unwrap();
+
+    Logger::new().init().expect("failed to initialize logger");
 
     let prepare_commit_msg = PrepareCommitMessage {
         commit_msg_file: Path::new(commit_msg_file),
