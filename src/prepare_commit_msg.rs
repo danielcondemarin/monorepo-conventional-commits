caa2322e6b070db3962b23d4979e4cb04f959e8e
danielconde <danielconde9@gmail.com>
2021-02-27 10:09:41 +0000

    test: create and use a helper to run a vec of test cases

--- src/prepare_commit_msg.rs
+++ src/prepare_commit_msg.rs
@@ -75,8 +75,24 @@ impl<'a> PrepareCommitMessage<'a> {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::io::SeekFrom;
+    use core::panic;
+    use std::{
+        io::SeekFrom,
+        panic::{catch_unwind, AssertUnwindSafe, UnwindSafe},
+    };
     use tempfile::NamedTempFile;
+
+    fn test_each<'a, T, A>(test_cases: Vec<A>, test: T)
+    where
+        T: Clone + Fn(A) -> () + UnwindSafe,
+        A: UnwindSafe,
+    {
+        for args in test_cases {
+            let test_clone = AssertUnwindSafe(test.clone());
+            let result = catch_unwind(|| test_clone(args));
+            assert!(result.is_ok())
+        }
+    }
 
     struct MockCommitScopeFinder {
         scopes: Vec<String>,
@@ -103,80 +119,85 @@ mod tests {
     }
 
     #[test]
-    fn ignores_unsupported_commit_sources() {
-        let unsupported_commit_sources = vec!["template", "squash", "commit", "anything_else"];
+    fn test_scenarios() {
+        struct TestCase<'a> {
+            description: &'a str,
+            commit_msg_contents: &'a str,
+            commit_source: Option<&'a str>,
+            scopes: Vec<&'a str>,
+            expected_commit: &'a str,
+        }
 
-        for src in unsupported_commit_sources {
-            let tmp_file = create_tmp_git_commit_file("some commit message");
+        let test_cases: Vec<TestCase> = vec![
+            TestCase {
+                description: "does not do anything when commit source = \"squash\"",
+                commit_source: Some("squash"),
+                scopes: vec!["app1"],
+                commit_msg_contents: "some commit message",
+                expected_commit: "some commit message",
+            },
+            TestCase {
+                description: "does not do anything when commit source = \"commit\"",
+                commit_source: Some("commit"),
+                scopes: vec!["app1"],
+                commit_msg_contents: "some commit message",
+                expected_commit: "some commit message",
+            },
+            TestCase {
+                description: "does not do anything when commit source = \"squash\"",
+                commit_source: Some("merge"),
+                scopes: vec!["app1"],
+                commit_msg_contents: "some commit message",
+                expected_commit: "some commit message",
+            },
+            TestCase {
+                description: "does not do anything when commit source = \"anything_else\"",
+                commit_source: Some("anything_else"),
+                scopes: vec!["app1"],
+                commit_msg_contents: "some commit message",
+                expected_commit: "some commit message",
+            },
+            TestCase {
+                description: "does not do anything when no scopes are affected",
+                commit_source: None,
+                scopes: vec![],
+                commit_msg_contents: "some commit message",
+                expected_commit: "some commit message",
+            },
+            TestCase {
+                description: "reuses commit type from user input",
+                commit_source: Some("message"),
+                scopes: vec!["package"],
+                commit_msg_contents: "docs: commit message with type specified already",
+                expected_commit: "docs(package): commit message with type specified already",
+            },
+            TestCase {
+                description:
+                    "adds generated commit message to the beginning of commit message file",
+                commit_source: None,
+                scopes: vec!["package"],
+                commit_msg_contents: "initial contents of commit message\nwith multiple lines",
+                expected_commit:
+                    "chore(package):\ninitial contents of commit message\nwith multiple lines",
+            },
+        ];
+
+        test_each(test_cases, |tc| {
+            println!("Running test: {}", tc.description);
+
+            let tmp_file = create_tmp_git_commit_file(tc.commit_msg_contents);
 
             let pcm = PrepareCommitMessage {
                 commit_msg_file: tmp_file.path(),
-                commit_source: Some(src),
+                commit_source: tc.commit_source,
                 commit_msg_generator: &MockCommitScopeFinder {
-                    scopes: vec!["app1".to_string()],
+                    scopes: tc.scopes.iter().map(|s| s.to_string()).collect(),
                 },
             };
 
             pcm.update_commit().unwrap();
 
-            assert_eq!(read_git_commit_file(tmp_file), "some commit message");
-        }
-    }
-
-    #[test]
-    fn doesnt_do_anything_when_no_scopes_affected() {
-        let tmp_file = create_tmp_git_commit_file("some commit message");
-
-        let pcm = PrepareCommitMessage {
-            commit_msg_file: tmp_file.path(),
-            commit_source: None,
-            commit_msg_generator: &MockCommitScopeFinder { scopes: vec![] },
-        };
-
-        pcm.update_commit().unwrap();
-
-        assert_eq!(read_git_commit_file(tmp_file), "some commit message");
-    }
-
-    #[test]
-    fn reuses_commit_type_from_user_input() {
-        let tmp_file =
-            create_tmp_git_commit_file("docs: commit message with type specified already");
-
-        let pcm = PrepareCommitMessage {
-            commit_msg_file: tmp_file.path(),
-            commit_source: Some("message"),
-            commit_msg_generator: &MockCommitScopeFinder {
-                scopes: vec!["package".to_string()],
-            },
-        };
-
-        pcm.update_commit().unwrap();
-
-        assert_eq!(
-            read_git_commit_file(tmp_file),
-            "docs(package): commit message with type specified already"
-        );
-    }
-
-    #[test]
-    fn prepends_generated_commit_message() {
-        let tmp_file =
-            create_tmp_git_commit_file("initial contents of commit message\nwith multiple lines");
-
-        let pcm = PrepareCommitMessage {
-            commit_msg_file: tmp_file.path(),
-            commit_source: None,
-            commit_msg_generator: &MockCommitScopeFinder {
-                scopes: vec!["package".to_string()],
-            },
-        };
-
-        pcm.update_commit().unwrap();
-
-        assert_eq!(
-            read_git_commit_file(tmp_file),
-            "chore(package):\ninitial contents of commit message\nwith multiple lines"
-        );
+            assert_eq!(read_git_commit_file(tmp_file), tc.expected_commit);
+        });
     }
 }
